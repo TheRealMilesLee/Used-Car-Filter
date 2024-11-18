6238ae63751597e2da1ff39420edb388cbd5ce53
Hengyi Li <trdli@ucdavis.edu>
2024-11-18 23:11:12 +0000

    Update the template

--- src/Graph3.js
+++ src/Graph3.js
@@ -1,5 +1,5 @@
 import * as d3 from 'd3';
-import { size } from "./VisualizeLayout.js";
+import { size } from "./Diagrams.js";
 import { column_from_csv } from './csvReadIn.js';
 import { Graph3_data_cleaning } from './graphDataCleaning.js';
 
---
+++ src/graphDataCleaning.js
@@ -0,0 +1,11 @@
+export const graph1_data_cleaning = () =>
+{
+};
+
+export const Graph2_data_cleaning = () =>
+{
+};
+
+export const Graph3_data_cleaning = () =>
+{
+};
