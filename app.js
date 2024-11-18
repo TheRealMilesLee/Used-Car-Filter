372756c6ac1177b076e94ecef80c61810fee0960
TheRealMilesLee <grandtheftauto710@outlook.com>
2024-11-18 18:16:43 +0000

    Update the strtuctre

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-import { Page1, Page2, Page3, Page4, Page5, Page6, Page7, Page8, Page9 } from './src/Pages';
-import './style.css';
+import "./style.css";
 
+document.querySelector('.nav-bar').innerHTML = `
+  <nav class="nav-bar">
+    <ul>
+  </nav>
+`;
+
+document.querySelector('#MainBody').innerHTML = `
+  <div class="main-body">
+    <h1>Welcome to my website!</h1>
+    <p>Here you can find a lot of interesting information about me and my projects.</p>
+  </div>
+`;
+
+document.querySelector('.footer').innerHTML = `
+  <footer class="footer">
+    <p>© 2024 Hengyi Li, Shu Zhang All rights reserved</p>
+  </footer>
+`;
 
