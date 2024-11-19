6a2069f80b2b13bf2ded4de2fade1b8655c65b5a
Hengyi Li <trdli@ucdavis.edu>
2024-11-19 00:47:14 +0000

    Update the CSV Read In files

--- src/csvReadIn.js
+++ src/csvReadIn.js
@@ -11,14 +11,14 @@ export let column_from_csv = await d3.csv('../Resources/car_data.csv', (d) =>
     return null;
   }
   return {
-    car_brand: d.car_brand.trim(),
-    car_model: d.car_model.trim(),
-    car_price: +d.car_price, // Convert price to number
-    car_city: d.car_city.trim(),
-    car_transmission: d.car_transmission.trim(),
-    car_mileage: +d.car_mileage, // Convert mileage to number
-    car_engine_capacity: +d.car_engine_capacity, // Convert engine capacity to number
-    car_age: +d.car_age // Convert age to number
+    brand: d.car_brand.trim(),
+    model: d.car_model.trim(),
+    price: +d.car_price, // Convert price to number
+    city_sold: d.car_city.trim(),
+    transmissio: d.car_transmission.trim(),
+    mileage: +d.car_mileage, // Convert mileage to number
+    engine_capacity: +d.car_engine_capacity, // Convert engine capacity to number
+    age: +d.car_age // Convert age to number
   };
 }).then(data =>
 {
