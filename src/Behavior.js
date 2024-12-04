ae01e06e04465d93eea93e44a54577455a2c7540
Hengyi Li <trdli@ucdavis.edu>
2024-12-04 01:39:30 +0000

    Update the behavior

--- src/Behavior.js
+++ src/Behavior.js
@@ -47,9 +47,6 @@ window.onload = () =>
       document.querySelector("#LineChart").style.display = "none";
       document.querySelector("#BarChart").style.display = "none";
       document.querySelector("#TransmissionBarChart").style.display = "none";
-      document.querySelector("#CityBrandChart").style.display = "none";
-      document.querySelector("#ModelSalesChart").style.display = "none";
-      document.querySelector("#EngineCategoryChart").style.display = "none";
     }
   };
 };
